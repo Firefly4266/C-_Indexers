2abc20ccdd69b44e6d110ae8c4646317594d252f
Firefly4266 <gaines.johnp@gmail.com>
2017-09-05 23:42:37 -0700

    Making cookie class for indexer example

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class HttpCookie
+    {
+        private readonly Dictionary<string, string> dictionary;
+        
+        public HttpCookie()
+        {
+            dictionary = new Dictionary<string, string>();
+        }
+        public string this[string key]
+        {
+            get { return dictionary[key]; }
+            set { dictionary[key] = value; }
+
+        }
+    }
+}
